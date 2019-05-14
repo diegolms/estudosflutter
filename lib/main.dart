08decd069c51efc2b41a06c1c06b16f9543f1988
Diego Lopes <diegolopes@Diegos-MacBook-Pro.local>
2019-05-14 06:14:14 -0300

    tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
+import 'package:hello_world/pages/login_page.dart';
+import 'package:hello_world/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+  };
+
   @override
   Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Welcome to Flutter',
+//      theme: ThemeData(primarySwatch: Colors.deepPurple),
+//      home: LoginPage(),
+//    );
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello Worldss'),
-        ),
+      title: 'Kodeversitas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        fontFamily: 'Nunito',
       ),
+      home: LoginPage(),
+      routes: routes,
     );
   }
 }
